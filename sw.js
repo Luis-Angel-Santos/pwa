4771fb720537cf8eac18eef93af311441be2fb34
Luis Angel Santos <santosangel748@gmail.com>
2022-10-02 15:48:40 -0500

    Manejo de errores en el fetch event

--- sw.js
+++ sw.js
@@ -1,5 +1,14 @@
 self.addEventListener('fetch', event => {
-    if(event.request.url.includes('main.jpg')){
+    /*if(event.request.url.includes('main.jpg')){
         event.respondWith(fetch('img/main-patas-arriba.jpg'));
-    }
+    }*/
+    const respuesta = fetch(event.request)
+                        .then(resp => {
+                            if(resp.ok){
+                                return resp;
+                            }else{
+                                return fetch('img/main.jpg');
+                            }
+                        });
+    event.respondWith(respuesta);
 });
