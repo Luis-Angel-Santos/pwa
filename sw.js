3619b82744628f3458cd35f4b37836bddd8d538b
Luis Angel Santos <santosangel748@gmail.com>
2022-10-02 15:27:17 -0500

    Trabajando con service worker - fetch event

--- sw.js
+++ sw.js
@@ -1,1 +1,7 @@
-console.log('sw');
+self.addEventListener('fetch', event => {
+    if(event.request.url.includes('style.css')){
+        event.respondWith(null);
+    }else{
+        event.respondWith(fetch(event.request));
+    }
+});
