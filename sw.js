3834fda6cb70e45968e26d5482b7b01e8cafa2bd
Luis Angel Santos <santosangel748@gmail.com>
2022-10-10 21:29:01 -0500

    Navegacion offline con pagina de error

--- sw.js
+++ sw.js
@@ -1,5 +1,4 @@
-//const CACHE_NAME = 'cache-1';
-const CACHE_STATIC_NAME = 'static-v2';
+const CACHE_STATIC_NAME = 'static-v1';
 const CACHE_DYNAMIC_NAME = 'dynamic-v1';
 const CACHE_INMUTABLE_NAME = 'inmutable-v1';
 const CACHE_DYNAMYX_LIMIT = 50;
@@ -26,6 +25,7 @@ self.addEventListener('install', e => {
             'img/main.jpg',
             '/js/app.js',
             '/img/no-image.jpg',
+            '/pages/offline.html',
         ]);
     });
     const cacheInmutable = caches.open(CACHE_INMUTABLE_NAME)
@@ -33,77 +33,20 @@ self.addEventListener('install', e => {
     e.waitUntil(Promise.all([cacheProm, cacheInmutable]));
 });
 
-self.addEventListener('fetch', e => {
-    //Cache & Network Race
-    const respuesta = new Promise((resolve, reject) => {
-        let rechazada = false;
-        const falloUnavez = () => {
-            if(rechazada){
-                if(/\.(png|jpg)$/i.test(e.request.url)){
-                    resolve(caches.match('/img/no-image.jpg'));
-                }else{
-                    reject('no se encontro respuesta')
-                }
-            }else{
-                rechazada = true;
-            }
-        }
-        fetch(e.request).then(res => {
-            res.ok ? resolve(res) : falloUnavez();
-        }).catch(falloUnavez);
-        caches.match(e.request).then(res => {
-            res ? resolve(res) : falloUnavez(); 
-        }).catch(falloUnavez);
-    });
-    
-    e.repondWith(respuesta);
-    //Cache Only
-    //e.respondWith(caches.match(e.request));
-
-    //Cache with network fallback
-    /*const respuestaCache = caches.match(e.request)
+self.addEventListener('fetch', e =>{
+    const respuestaCache = caches.match(e.request)
         .then(resp => {
             if(resp) return resp;
             //no existe archivo
-            console.log('No existe', e.request.url);
             return fetch(e.request).then(newResp => {
                 caches.open(CACHE_DYNAMIC_NAME)
                     .then(cache => {
                         cache.put(e.request, newResp);
-                        limpiarCache(CACHE_DYNAMIC_NAME, 10); 
+                        limpiarCache(CACHE_DYNAMIC_NAME, CACHE_DYNAMYX_LIMIT); 
                     });    
                 return newResp.clone();
             });
         });
 
     e.respondWith(respuestaCache);
-    */
-
-    //Network with cache fallback
-    /*const respuesta = fetch(e.request).then(res =>{
-        console.log('fetch', res);
-        caches.open(CACHE_DYNAMIC_NAME)
-            .then(cache => {
-                cache.put(e.request, res);
-                limpiarCache(CACHE_DYNAMIC_NAME, CACHE_DYNAMYX_LIMIT);
-            });
-        return res.clone();
-    }).catch(error => {
-        return caches.match(e.request);
-    });
-
-    e.respondWith(respuesta);*/
-    //Cache with network update
-    /*if(e.request.url.includes('bootstrap')){
-        return respondWith(caches.match(e.request));
-    }
-    const respuesta = caches.open(CACHE_STATIC_NAME).then(cache => {
-        fetch(e.request).then(newResp => 
-            cache.put(e.request, newResp));
-        return cache.match(e.request);
-    })
-
-    e.respondWith(respuesta);*/
-    
-
 });
